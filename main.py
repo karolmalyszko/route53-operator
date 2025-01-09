fcb630c6168d3bbac3bd7656d9085cf1bacd276c
Karol Małyszko <karol.malyszko@lite.tech>
2025-01-09 19:56:17 +0100

    add exception handling

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ logging.basicConfig(
     format="{asctime} - {levelname} - {message}",
     style="{",
     datefmt="%d-%m-%Y %H:%M",
-    level=logging.INFO,
+    level=logging.DEBUG,
+    # level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
@@ -17,62 +18,109 @@ subdomainList = ['test']
 
 def getRecordValue( subdomain ):
     logger.debug("Getting DNS record value.")
-    rsp = client.list_resource_record_sets(
-        HostedZoneId=hostedZoneID,
-        StartRecordName="{}.{}".format(subdomain, domainName),
-        StartRecordType='A',
-        MaxItems='1'
-    )
+    status = False
+    try:
+        rsp = client.list_resource_record_sets(
+            HostedZoneId=hostedZoneID,
+            StartRecordName="{}.{}".format(subdomain, domainName),
+            StartRecordType='A',
+            MaxItems='1'
+        )
+        status = True
+    except:
+        logger.error("Failed getting DNS record value for {}".format(subdomain))
 
-    return json.dumps(rsp["ResourceRecordSets"][0]["ResourceRecords"][0]["Value"]).strip("\"")
-    # print(json.dumps(rsp["ResourceRecordSets"], indent=4))
-    # print(ip)
+    if status:
+        logger.debug("DNS record value retrieved")
+        return True, json.dumps(rsp["ResourceRecordSets"][0]["ResourceRecords"][0]["Value"]).strip("\"")
+    else:
+        return False, None
 
 def getHostedZoneID( zoneName ):
     logger.debug("Getting hosted DNS zone ID for further operations")
-    rsp = client.list_hosted_zones_by_name(
-        DNSName=zoneName,
-        MaxItems='1'
-    )
+    status = False
+    try:
+        rsp = client.list_hosted_zones_by_name(
+            DNSName=zoneName,
+            MaxItems='1'
+        )
+        status = True
+    except:
+        logger.error("Invalid domain name provided")
 
-    return json.dumps(rsp["HostedZones"][0]["Id"]).split("/", 2)[2].strip("\"")
+    if status:
+        logger.debug("Hosted zone ID retrieved.")
+        return True, json.dumps(rsp["HostedZones"][0]["Id"]).split("/", 2)[2].strip("\"")
+    else:
+        return False, None
 
 def updateRecordValue( subdomain, newIp ):
     logger.info("Changing '{}' record to {}".format(subdomain, newIp))
-    rsp = client.change_resource_record_sets(
-        ChangeBatch={
-            'Changes': [
-                {
-                    'Action': 'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': '{}.{}'.format(subdomain, domainName),
-                        'ResourceRecords': [
-                            {
-                                'Value': '{}'.format(newIp),
-                            },
-                        ],
-                        'TTL': 60,
-                        'Type': 'A',
+    status = False
+    try:
+        rsp = client.change_resource_record_sets(
+            ChangeBatch={
+                'Changes': [
+                    {
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': '{}.{}'.format(subdomain, domainName),
+                            'ResourceRecords': [
+                                {
+                                    'Value': '{}'.format(newIp),
+                                },
+                            ],
+                            'TTL': 60,
+                            'Type': 'A',
+                        },
                     },
-                },
-            ]
-        },
-        HostedZoneId=hostedZoneID,
-    )
-    changeId = json.dumps(rsp["ChangeInfo"]["Id"]).strip("\"")
-    changeStatus = json.dumps(rsp["ChangeInfo"]["Status"]).strip("\"")
+                ]
+            },
+            HostedZoneId=hostedZoneID,
+        )
+        changeId = json.dumps(rsp["ChangeInfo"]["Id"]).strip("\"")
+        changeStatus = json.dumps(rsp["ChangeInfo"]["Status"]).strip("\"")
+    except:
+        logger.error("Changing DNS record for {} failed!".format(subdomain))
 
-    return changeId, changeStatus
+    if status:
+        logger.debug("DNS record for {} updated".format(subdomain))
+        return True, changeId, changeStatus
+    else:
+        return False, None, None
 
-hostedZoneID = getHostedZoneID( domainName )
-currentIp = requests.get('https://ifconfig.me').text
+def getCurrentIP():
+    logging.debug("Getting current IP address from ifconfig.me")
+    status = False
+    try:
+        ip = requests.get('https://ifconfig.me').text
+        status = True
+    except:
+        logger.error("Something went wrong calling ifconfig.me")
+
+    if status:
+        logger.debug("Current IP retrieved")
+        return True, ip
+    else:
+        return False, None
+
+#======== start of script execution
+status, id = getHostedZoneID(domainName)
+if status:
+    hostedZoneID = id
+
+status, ip = getCurrentIP()
+if status:
+    currentIp = ip
 
 for subdomain in subdomainList:
     logger.debug("Checking for differences")
-    remoteIp = getRecordValue( subdomain )
-    if remoteIp != currentIp:
-        logger.info("Changes found for '{}' subdomain".format(subdomain))
-        id, status = updateRecordValue(subdomain, currentIp)
+    status, remoteIp = getRecordValue(subdomain)
+    if status:
+        if remoteIp != currentIp:
+            logger.info("Changes found for '{}' subdomain".format(subdomain))
+            changeID, changeStatus = updateRecordValue(subdomain, currentIp)
+        else: logger.info("No changes for '{}' subdomain found".format(subdomain))
     # status.append(partial)
 
 # photosIp = getRecordValue( "photos" )
@@ -94,10 +142,6 @@ for subdomain in subdomainList:
 # print(response)
 
 # # TODO
-# dodać łowienie exceptions przy wywołaniach funkcji zarządzających Route53
-# dodać logowanie
-#     - do konsoli
-#     - do pliku
 # dodać powiadamianie mailowe za pomocą AWS SES
 # nazwa domeny podawana jako zmienna środowiskowa / z poziomu pliku konfiguracyjnego
 # subdomeny podawane jako zmienne środowiskowe / z poziomu pliku konfiguracyjnego
